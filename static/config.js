460cd992b790b477384754717db6ba4d35b132b6
wangxin <553613117@qq.com>
2020-07-23 09:24:31 +0800

    Update config.js

--- static/config.js
+++ static/config.js
@@ -1,6 +1,6 @@
 window.apiConfig = {
-  server_url: "http://39.97.165.22:91",
-  appID: "wx521fb48cf8db0833",
+  server_url: "http://39.97.165.23:91",//接口地址
+  appID: "wx521fb48cf8db0134",//appid
   redirect_url: "http://mq.lylhy.cn/#/auth",
   redirect_url1: "http://mq.lylhy.cn/#/bindInfo",
   wx_url: 'https://open.weixin.qq.com/connect/oauth2/authorize'
